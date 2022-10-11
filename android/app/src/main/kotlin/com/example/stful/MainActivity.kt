08ef4d0f6fbcab00a644fe59741f87a7533067e8
mustopaa <amsualamm@gmail.com>
2022-10-12 06:52:19 +0700

    Latihan Stateful

---
+++ android/app/src/main/kotlin/com/example/stful/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stful
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int number =1;
+
+  void tekanTombol(){
+    setState(() {
+      number = number+1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Stateful"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(number.toString()),
+              MaterialButton(
+                color: Colors.red,
+                height: 50,
+                shape: RoundedRectangleBorder(borderRadius:new BorderRadius.circular(5.0)),
+                onPressed: tekanTombol,
+                child: Text("Tambah"),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
